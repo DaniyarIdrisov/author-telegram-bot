e6a0ebbd6c1954efd6bbb344d1073061acdc3fc1
Идрисов Данияр Фаритович DaFIdrisov <dafidrisov@stud.kpfu.ru>
2025-01-27 23:26:50 +0300

    adding repository

---
+++ src/main/java/ru/kpfu/itis/daniyar/idrisov/authortelegrambot/AuthorTelegramBotApplication.java
@@ -0,0 +1,12 @@
+package ru.kpfu.itis.daniyar.idrisov.authortelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorTelegramBotApplication.class, args);
+    }
+}
