ff0d17b84a4d01bb1856cd6efb82c6c1401d939b
Идрисов Данияр Фаритович DaFIdrisov <dafidrisov@stud.kpfu.ru>
2025-01-29 00:01:14 +0300

    added callbacks

--- src/main/java/ru/kpfu/itis/daniyar/idrisov/authortelegrambot/telegram/handlers/callbacks/GetDisputeFileByTypeCallback.java
+++ src/main/java/ru/kpfu/itis/daniyar/idrisov/authortelegrambot/telegram/handlers/callbacks/GetDisputeFileByTypeCallback.java
@@ -6,9 +6,13 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.kpfu.itis.daniyar.idrisov.authortelegrambot.models.enums.DisputeType;
 import ru.kpfu.itis.daniyar.idrisov.authortelegrambot.services.DisputeService;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,10 +24,14 @@ public class GetDisputeFileByTypeCallback implements Callback {
 
     @Override
     public SendDocument apply(Update update) {
-        var disputeType = update.getCallbackQuery().getData();
+        var disputeType = DisputeType.valueOf(update.getCallbackQuery().getData());
         long chatId = update.getCallbackQuery().getMessage().getChatId();
-        var file = disputeService.createXlsByType(DisputeType.valueOf(disputeType));
+        var file = disputeService.createXlsByType(disputeType);
+        InputStream fileInputStream = new ByteArrayInputStream(file);
+
         var sendDocument = new SendDocument();
         sendDocument.setChatId(chatId);
+        sendDocument.setDocument(new InputFile(fileInputStream, disputeType.getTypeValue() + ".xls"));
+        return sendDocument;
     }
 }
