d442cc7c1770cce2c8504e153048689a7af5f03a
Идрисов Данияр Фаритович DaFIdrisov <dafidrisov@stud.kpfu.ru>
2025-02-14 21:11:20 +0300

    added v.3

--- src/main/java/ru/kpfu/itis/daniyar/idrisov/authortelegrambot/telegram/handlers/callbacks/GetDisputePerformingCurrentFileCallback.java
+++ src/main/java/ru/kpfu/itis/daniyar/idrisov/authortelegrambot/telegram/handlers/callbacks/GetDisputePerformingCurrentFileCallback.java
@@ -6,8 +6,13 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.kpfu.itis.daniyar.idrisov.authortelegrambot.services.DisputeService;
+import ru.kpfu.itis.daniyar.idrisov.authortelegrambot.telegram.constants.TelegramConstants;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,7 +24,13 @@ public class GetDisputePerformingCurrentFileCallback implements Callback {
 
     @Override
     public SendDocument apply(Update update) {
-        //TODO
-        return null;
+        long chatId = update.getCallbackQuery().getMessage().getChatId();
+        var file = disputeService.createXlsDisputePerformingCurrent();
+        InputStream fileInputStream = new ByteArrayInputStream(file);
+
+        var sendDocument = new SendDocument();
+        sendDocument.setChatId(chatId);
+        sendDocument.setDocument(new InputFile(fileInputStream,  TelegramConstants.DISCUSSION_PERFORMING_CURRENT_CALLBACK_DATA + ".xls"));
+        return sendDocument;
     }
 }
